c18f0a7cc72097905c5246d1f051125427aa3183
Abdelrahman Ramadan <abdoksa71@gmail.com>
2026-09-21 04:00:34 +0300

    Update bellabeat_analysis.sql

--- bellabeat_analysis.sql
+++ bellabeat_analysis.sql
@@ -1,4 +1,8 @@
--- Summary statistics
+-- Bellabeat Smart Device Usage Analysis
+-- Analyze Stage
+
+
+-- 1. Summary Statistics
 SELECT
   AVG(TotalSteps) AS avg_steps,
   AVG(TotalDistance) AS avg_distance,
@@ -10,3 +14,51 @@ SELECT
   MAX(TotalDistance) AS max_distance,
   MAX(Calories) AS max_calories
 FROM `project-e48f5d78-a5e7-45de-a67.Bellabeat.dailyActivity`;
+
+
+-- 2. Average Steps by Day of Week
+SELECT
+  FORMAT_DATE('%A', ActivityDate) AS day_of_week,
+  AVG(TotalSteps) AS avg_steps
+FROM `project-e48f5d78-a5e7-45de-a67.Bellabeat.dailyActivity`
+GROUP BY day_of_week
+ORDER BY avg_steps DESC;
+
+
+-- 3. Average Steps and Calories by Day of Week
+SELECT
+  FORMAT_DATE('%A', ActivityDate) AS day_of_week,
+  AVG(TotalSteps) AS avg_steps,
+  AVG(Calories) AS avg_calories
+FROM `project-e48f5d78-a5e7-45de-a67.Bellabeat.dailyActivity`
+GROUP BY day_of_week
+ORDER BY avg_steps DESC;
+
+
+-- 4. Relationship Between Steps and Calories
+SELECT
+  CORR(TotalSteps, Calories) AS correlation
+FROM `project-e48f5d78-a5e7-45de-a67.Bellabeat.dailyActivity`;
+
+
+-- 5. Activity Minutes by Day of Week
+SELECT
+  FORMAT_DATE('%A', ActivityDate) AS day_of_week,
+  AVG(VeryActiveMinutes) AS avg_very_active_minutes,
+  AVG(FairlyActiveMinutes) AS avg_fairly_active_minutes,
+  AVG(LightlyActiveMinutes) AS avg_lightly_active_minutes
+FROM `project-e48f5d78-a5e7-45de-a67.Bellabeat.dailyActivity`
+GROUP BY day_of_week
+ORDER BY day_of_week DESC;
+
+
+-- 6. Save Weekday Summary for Visualization
+CREATE OR REPLACE TABLE
+`project-e48f5d78-a5e7-45de-a67.Bellabeat.daily_summary` AS
+SELECT
+  FORMAT_DATE('%A', ActivityDate) AS day_of_week,
+  AVG(TotalSteps) AS avg_steps,
+  AVG(Calories) AS avg_calories
+FROM `project-e48f5d78-a5e7-45de-a67.Bellabeat.dailyActivity`
+GROUP BY day_of_week
+ORDER BY avg_steps DESC;
